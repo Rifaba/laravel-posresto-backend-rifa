a03e98ab3b4767d38d068fec43058fa8fc8b7ddc
Nur Muhammad <blog.nurmuhammad@gmail.com>
2024-02-18 14:46:59 +0800

    Fix logout form closing tag

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -265,12 +265,12 @@
                 <a href="#"
                     class="dropdown-item has-icon text-danger"
                     onclick="event.preventDefault(); document.getElementById('logout-form').submit()">
-            
+
                     <i class="fas fa-sign-out-alt"></i> Logout
                 </a>
                 <form id="logout-form" action="{{route('logout')}}" method="POST" style="display: none:">
                     @csrf
-                <form/>
+                </form>
             </div>
         </li>
     </ul>
